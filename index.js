a3c30a2d8e99411c59107da1dde4114a011a54d5
Arunabh Saikia <developer.ariyanas@gmail.com>
2020-02-06 23:35:46 +0530

    Added materialize css. Basic laout in index.html file done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const SERVER_PORT = process.env.SERVER_PORT || 5000
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello test'))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 app.listen(SERVER_PORT, () => console.log(`Server started at ${SERVER_PORT}`))
