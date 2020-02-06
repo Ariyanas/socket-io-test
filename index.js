6217a4f7567d23fd2c9d878b96ba6cc76c2f8131
Arunabh Saikia <developer.ariyanas@gmail.com>
2020-02-06 23:47:53 +0530

    Basic socket event handling done

--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const SERVER_PORT = process.env.SERVER_PORT || 5000
 
 const app = express()
 
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile('index.html'))
 
-app.listen(SERVER_PORT, () => console.log(`Server started at ${SERVER_PORT}`))
+// Socket IO setup
+io.on('connection', (socket) => {
+    console.log('Client connected')
+
+    socket.on('disconnect', () => console.log('Client disconnected'))
+})
+
+// app.listen(SERVER_PORT, () => console.log(`Server started at ${SERVER_PORT}`))
+server.listen(SERVER_PORT, () => console.log(`Server started at ${SERVER_PORT}`))
